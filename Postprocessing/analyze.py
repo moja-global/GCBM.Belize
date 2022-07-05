b1fa0debaa9f8b9ff6e91efaf5fb248306745f0b
radis toubalidis <rtoumpalidis@gmail.com>
2022-07-05 15:44:05 +0300

    updates analyze.py to calculate metrics for every 50 years and adds it to post_processing metrics in dvc
    Signed-off-by: radis toubalidis <rtoumpalidis@gmail.com>

--- Postprocessing/analyze.py
+++ Postprocessing/analyze.py
@@ -1,38 +1,50 @@
-from statistics import mean
-import pandas as pd
 import os
+import pandas as pd
 import json
 
-dom_sensitivity_full = pd.read_csv(os.path.join("Tables","Pools_DOM_Sensitivity_full.csv"))
+pools = pd.read_csv(os.path.join('Tables','Pools_DOM_Sensitivity_full.csv'))
+pools_divided = [
+    {'period':'1900-1950','data':pools[pools['year']<=1950]},
+    {'period':'1951-2000','data':pools[(pools['year']>1950) & (pools['year']<=2000)]},
+    {'period':'2001-2050','data':pools[pools['year']>2000]}
+    ]
 
-def calc_metrics(ind_type):
-    indicator = dom_sensitivity_full[dom_sensitivity_full['indicator'] == ind_type]
+def calc_metrics(pool,ind_type):
+    indicator = pool[pool['indicator'] == ind_type]
     dry = indicator[indicator['LifeZone'] == 'Tropical Dry']
     moist = indicator[indicator['LifeZone'] == 'Tropical Moist']
     wet = indicator[indicator['LifeZone'] == 'Tropical Premontane Wet']
-
     metrics = {
-        f"{ind_type}, Tropical Dry":{
+        'indicator':ind_type,
+        "Tropical Dry":{
             'pool_tc_sum_MEAN' : format(dry['pool_tc_sum'].mean(),'.2f'),
             'area_sum_MEAN' : format(dry['area_sum'].mean(),'.2f'),
-            'pool_tc_per_ha_MEAN': format(dry['pool_tc_per_ha'].mean(),'.2f'),
+            'pool_tc_per_ha_MEAN': format(dry['pool_tc_per_ha'].mean(),'.2f')
         },
-        f"{ind_type}, Tropical Moist":{
+        "Tropical Moist":{
             'pool_tc_sum_MEAN' : format(moist['pool_tc_sum'].mean(),'.2f'),
             'area_sum_MEAN' : format(moist['area_sum'].mean(),'.2f'),
             'pool_tc_per_ha_MEAN': format(moist['pool_tc_per_ha'].mean(),'.2f'),
         },
-        f"{ind_type}, Tropical Premontane Wet":{
+        "Tropical Premontane Wet":{
             'pool_tc_sum_MEAN' : format(wet['pool_tc_sum'].mean(),'.2f'),
             'area_sum_MEAN' : format(wet['area_sum'].mean(),'.2f'),
             'pool_tc_per_ha_MEAN': format(wet['pool_tc_per_ha'].mean(),'.2f')
         }
     }
-    formated_ind_type = ind_type.replace(' ','_')
-    with open(os.path.join('metrics',f'{formated_ind_type}.json'),'w') as out:
-        json.dump(metrics,out)
+    return metrics
 
-if __name__ == "__main__":
-    indicators = dom_sensitivity_full['indicator'].unique()
+if __name__ == '__main__':
+    indicators = pools['indicator'].unique()
+    metrics = []
+    for q in pools_divided:
+        metrics.append(
+            {'period':q['period'],'data':[]}
+        )
     for ind_type in indicators:
-        calc_metrics(ind_type)
+        for i,q_metrics in enumerate(metrics):
+            new_item = calc_metrics(pools_divided[i]['data'],ind_type)
+            q_metrics['data'].append(new_item)
+            with open(os.path.join('metrics',f"{q_metrics['period']}.json"),'w') as out:
+                json.dump(q_metrics,out,indent=4)
+                
